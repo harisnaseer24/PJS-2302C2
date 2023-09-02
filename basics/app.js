705aea284e28d306630a9655a6ca9d060e9a9677
harisnaseer24 <harisnaseer258@gmail.com>
2023-09-02 13:06:47 +0500

    Javascript Class work

---
+++ basics/app.js
@@ -0,0 +1,54 @@
+//Variables
+// const pi=3.142;
+// pi=22/7;
+
+
+// let a=18;
+//  let a=14;
+// var b=45;
+// var b=44;
+
+// console.log(b)
+let age=18;
+// if(age<18){
+
+// alert("bye")
+
+// }
+
+// else if(age==18){
+//     alert("Kuch bhi nahi")
+// }
+// else{
+
+//     alert("hi")
+
+// }
+
+
+
+let isGreen=true;
+
+// let name=prompt("enter your name","user");
+// alert("hi " + name +" Your age is : "+age);
+
+
+// console.log(typeof(age))
+// console.log(typeof(isGreen))
+// let abc=+"123"
+// console.log(typeof(abc))
+// console.log("the answer of sum is: " +(25 +4))
+// console.log(name)
+
+//DOM
+let image=document.getElementById("image")
+function changeimg(){
+    image.src="./images/2.jpg";
+    image.style.border="2px solid red";
+}
+
+
+function reverseimg(){
+    image.src="./images/1.jpg";
+    image.style.border="2px solid black";
+}
